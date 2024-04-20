08068dfe296168c61cf937e07b30cfbb633fe226
chavdar7 <furkanchavdar@gmail.com>
2024-04-20 22:39:34 +0300

    learn1 eklendi

---
+++ learn1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char name[] = "furkan";
+    char name2[] = "furkan2";
+
+    printf("%d\n", *name);
+    printf("%d\n", *name2);
+
+
+    return 0;
+}
